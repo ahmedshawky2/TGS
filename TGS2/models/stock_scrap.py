b6ba86accadf4c0300b70e83fcc7f91a9003fa06
ahmedshawky2 <46525840+ahmedshawky2@users.noreply.github.com>
2020-04-22 18:17:55 +0200

    add reasons

--- TGS2/models/stock_scrap.py
+++ TGS2/models/stock_scrap.py
@@ -11,5 +11,5 @@ _logger = logging.getLogger(__name__)
 
 class lov (models.Model):
     _inherit = 'stock.scrap'
-    x_scrap_reason = fields.Selection([('Reason1', 'Reason1'),('Reason2','Reason2')], string='Scrap Reason')
+    x_scrap_reason = fields.Selection([('Expired', 'Expired'),('Damage','Damage')], string='Scrap Reason')
    
